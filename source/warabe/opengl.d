eb5f86c0f42d2cbdbc4e314df9a5d4aefb2752b5
outlandkarasu <outland.karasu@gmail.com>
2019-01-06 17:28:13 +0900

    issue #25 add OpenGL module.

---
+++ source/warabe/opengl.d
@@ -0,0 +1,18 @@
+module warabe.opengl;
+
+/**
+OpenGL related exception.
+*/
+class OpenGLException : WarabeException
+{
+    /// constructor from super class.
+    pure nothrow @nogc @safe this(
+            string msg,
+            string file = __FILE__,
+            size_t line = __LINE__,
+            Throwable nextInChain = null)
+    {
+        super(msg, file, line, nextInChain);
+    }
+}
+
