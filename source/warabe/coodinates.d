f3126ddb81aaa8a85fc561f3a2ade8b4ab01f556
outlandkarasu <outland.karasu@gmail.com>
2019-01-09 23:45:33 +0900

    issue #25 add basic coodinates types.

---
+++ source/warabe/coodinates.d
@@ -0,0 +1,23 @@
+module warabe.coodinates;
+
+///
+struct Point
+{
+    int x;
+    int y;
+}
+
+///
+struct Size
+{
+    uint width;
+    uint height;
+}
+
+///
+struct Rectangle
+{
+    Point position;
+    Size size;
+}
+
