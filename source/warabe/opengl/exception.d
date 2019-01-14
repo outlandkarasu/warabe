01346f3c3996654c1cd0221c6ec465e94835682b
outlandkarasu <outland.karasu@gmail.com>
2019-01-14 11:15:53 +0900

    issue #25 add OpenGL check error function.

--- source/warabe/opengl/exception.d
+++ source/warabe/opengl/exception.d
@@ -2,6 +2,12 @@ module warabe.opengl.exception;
 
 import warabe.exception : WarabeException;
 
+import std.conv : to;
+
+import bindbc.opengl :
+    GLenum,
+    glGetError;
+
 /**
 OpenGL related exception.
 */
@@ -15,6 +21,50 @@ class OpenGLException : WarabeException
             Throwable nextInChain = null)
     {
         super(msg, file, line, nextInChain);
+    }
+}
+
+/**
+ *  check glGetError result.
+ *
+ *  Params:
+ *      file = source file name.
+ *      line = source line number.
+ *  Throws:
+ *      OpenGLException if has OpenGL errors.
+ */
+void checkGLError(string file = __FILE__, size_t line = __LINE__)()
+{
+    checkGLError!(file, line)(glGetError());
+}
+
+/**
+ *  check glGetError result.
+ *
+ *  Params:
+ *      file = source file name.
+ *      line = source line number.
+ *      errorCode = OpenGL error code.
+ *  Throws:
+ *      OpenGLException if has OpenGL errors.
+ */
+void checkGLError(string file = __FILE__, size_t line = __LINE__)(GLenum errorCode)
+{
+    switch(errorCode) {
+        case GL_NO_ERROR:
+            return;
+        case GL_INVALID_ENUM:
+            throw new OpenGLException("GL_INVALID_ENUM", file, line);
+        case GL_INVALID_VALUE:
+            throw new OpenGLException("GL_INVALID_VALUE", file, line);
+        case GL_INVALID_OPERATION:
+            throw new OpenGLException("GL_INVALID_OPERATION", file, line);
+        case GL_INVALID_FRAMEBUFFER_OPERATION:
+            throw new OpenGLException("GL_INVALID_FRAMEBUFFER_OPERATION", file, line);
+        case GL_OUT_OF_MEMORY:
+            throw new OpenGLException("GL_OUT_OF_MEMORY", file, line);
+        default:
+            throw new OpenGLException("Unknown OpenGL error. " + errorCode.to!string, file, line);
     }
 }
 
